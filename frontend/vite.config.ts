edaf12137b727ea8381be860600571a3a93db4c7
José David Ramírez Rojas <josedavidrr@ciencias.unam.mx>
2022-03-08 21:12:24 -0600

    setting correct base for deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CUME/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
