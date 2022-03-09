8ec587d50d99f781ea101044a5cc76cce4bb0bb4
JoseRamirezRojas <josedavidrr@ciencias.unam.mx>
2022-03-09 02:52:52 -0600

    Restructured project w/new back&front-end versions

---
+++ src/main/java/com/ciencias/CUME/api/rest/MessageController.java
@@ -0,0 +1,16 @@
+package com.ciencias.CUME.api.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Bienvenido a CUME con Spring + Vue.js! ";
+    }
+
+}
---
+++ src/test/java/com/ciencias/CUME/CumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciencias.CUME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
